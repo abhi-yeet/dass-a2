d2672ab336241df2210ac41eb130fd83821f3ca3
abhi-yeet <gta5abisuhas@gmail.com>
2026-03-22 21:16:13 +0530

    Error 4: Fixed move() to award Go salary when passing Go as well as landing on it

--- whitebox/code/moneypoly/player.py
+++ whitebox/code/moneypoly/player.py
@@ -43,11 +43,15 @@ class Player:  # pylint: disable=too-many-instance-attributes
         Awards the Go salary if the player passes or lands on Go.
         Returns the new board position.
         """
+        old_position = self.position
         self.position = (self.position + steps) % BOARD_SIZE
 
-        if self.position == 0:
+        if old_position + steps >= BOARD_SIZE:
             self.add_money(GO_SALARY)
-            print(f"  {self.name} landed on Go and collected ${GO_SALARY}.")
+            if self.position == 0:
+                print(f"  {self.name} landed on Go and collected ${GO_SALARY}.")
+            else:
+                print(f"  {self.name} passed Go and collected ${GO_SALARY}.")
 
         return self.position
 
